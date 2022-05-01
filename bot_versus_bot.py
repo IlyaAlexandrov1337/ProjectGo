0e017f45ac8f6f4de548dbf25c513df55587cc30
WarBr1nger <ilya.alexandrv@g,ail.com>
2022-05-01 19:17:25 +0300

    Исправлены импорты, изменён код для игры bot vs bot, исправлен README

--- bot_versus_bot.py
+++ bot_versus_bot.py
@@ -7,24 +7,32 @@ import time
 
 
 def main():
+    dct = {}
     board_size = 19
     game = goboard.GameState.new_game(board_size)
     bots = {
         gotypes.Player.black: agent.naive.RandomBot(),
         gotypes.Player.white: agent.naive.RandomBot(),
     }
-    while not game.is_over():
-        # Установка таймера на 0,3 секунды обеспечивает задержку между отображением ходов ботов.
-        time.sleep(0.3)
+    for i in range(50):
+        game = goboard.GameState.new_game(board_size)
+        while not game.is_over():
+            # Установка таймера на 0,3 секунды обеспечивает задержку между отображением ходов ботов.
+            # time.sleep(0.3)
 
-        # Перед выполнением каждого хода экран очищается, благодаря чему
-        # доска всегда отображается в одном и том же месте командной строки.
-        print(chr(27) + "[2J")
-        print_board(game.board)
-        bot_move = bots[game.next_player].select_move(game)
-        print_move(game.next_player, bot_move)
-        game = game.apply_move(bot_move)
-    print(f"Winner: {game.winner()}")
+            # Перед выполнением каждого хода экран очищается, благодаря чему
+            # доска всегда отображается в одном и том же месте командной строки.
+            #print(chr(27) + "[2J")
+            #print_board(game.board)
+            bot_move = bots[game.next_player].select_move(game)
+            #print_move(game.next_player, bot_move)
+            game = game.apply_move(bot_move)
+        print(f"Winner: {game.winner()}")
+        if dct.get(game.winner()):
+            dct[game.winner()] += 1
+        else:
+            dct[game.winner()] = 1
+    print(dct)
 
 
 if __name__ == '__main__':
